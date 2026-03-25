bb00b0210c2b0248928be65f4e5655de3e4805e3
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-03-25 14:51:33 -0300

    changed display settings

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 let gridQuantity = 0; //Quantidade base de grids para ser alterada
 let divToChange = document.getElementsByClassName('gameGrids');
+let dadDiv = document.getElementById('squareGrid');
+let squareSize = 960 / 16;
 
 // Função de criação de novas divs
 function newDiv() {
-    const dadDiv = document.getElementById('squareGrid');
     const createdDiv = document.createElement('div');
     createdDiv.className = 'gameGrids';
     dadDiv.appendChild(createdDiv);
+
+    // Evento de mudança de cores.
+    createdDiv.addEventListener('mouseover', function () {
+            this.style.backgroundColor = '#000000'
+        });
 };
 
 // Função que define o tamanho do grid
-function baseGridSize() {
+function baseGridQuantity() {
     gridQuantity = 256 - 1;
     for (let i = 1; i <= gridQuantity; i++) {
         newDiv();
-    }
-}
-
-// Função de troca de cores das divs
-function changeColor() {
-    for (let i = 0; i < divToChange.length; i++)
-        divToChange[i].addEventListener('mouseover', function () {
-            this.style.backgroundColor = '#000000'
-        });
+    };
 };
 
 // Função de resetar a cor do grid
@@ -34,6 +32,23 @@ function resetGrid() {
     });
 };
 
-baseGridSize();
-changeColor();
+function changeGridSize() {
+    const getGridButton = document.getElementById('gridButton');
+    getGridButton.addEventListener('click', function(){
+        const userPrompt = parseInt(prompt('Change the size of the number of squares per side that you want. (Max. 100)'));
+        const removeGrid = document.getElementById('squareGrid').innerHTML = ""
+        gridQuantity = userPrompt * userPrompt;
+        squareSize = 960 / userPrompt;
+        dadDiv.style.gridTemplateColumns = `repeat(${userPrompt}, ${userPrompt}px)`;
+        divToChange[i].style.height = `${userPrompt}px`
+        divToChange[i].style.width = `${userPrompt}px`
+        for (let i = 1; i <= gridQuantity; i++) {
+            newDiv();
+
+    };
+    });
+};
+
+baseGridQuantity();
 resetGrid();
+changeGridSize();
