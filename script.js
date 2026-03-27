ebe877be2ccc54f0f1b1f6d6997c403a9af03af1
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-03-26 23:40:07 -0300

    add new buttons and functions

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let gridQuantity = 0; //Quantidade base de grids para ser alterada
 let divToChange = document.getElementsByClassName('gameGrids');
 let dadDiv = document.getElementById('squareGrid');
 let squareSize = 960 / 16;
+let activeButton = 'black'
+
 
 // Função de criação de novas divs
 function newDiv() {
@@ -10,9 +12,21 @@ function newDiv() {
     dadDiv.appendChild(createdDiv);
     createdDiv.style.width = `${squareSize}px`;
     createdDiv.style.height = `${squareSize}px`;
+    
     // Evento de mudança de cores.
     createdDiv.addEventListener('mouseover', function () {
-            this.style.backgroundColor = '#000000'
+        let divOpacity = this.style.opacity || '1'; 
+        if (activeButton === 'black') {
+            this.style.backgroundColor = '#000000' 
+        } else if (activeButton === 'random') {
+                    const red = Math.floor(Math.random() * 256);
+                    const green = Math.floor(Math.random() * 256);
+                    const blue = Math.floor(Math.random() * 256);
+                    this.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        } else if (activeButton === 'opacity') {  
+                let newDivOpacity = parseFloat(divOpacity) - 0.1;
+                this.style.opacity = `${newDivOpacity}`; 
+            };
         });
 };
 
@@ -27,9 +41,11 @@ function baseGridQuantity() {
 // Função de resetar a cor do grid
 function resetGrid() {
     const getResetButton = document.getElementById('resetButton');
-    getResetButton.addEventListener('click', function() { 
+    getResetButton.addEventListener('click', function() {
+        activeButton = 'black'
         for (let i = 0; i < divToChange.length; i++) {
-            divToChange[i].style.backgroundColor = '#ffffff'; };
+            divToChange[i].style.backgroundColor = '#ffffff';
+            divToChange[i].style.opacity = '1'};
     });
 };
 
@@ -47,6 +63,30 @@ function changeGridSize() {
     });
 };
 
+function opacityChange() {
+    const getOpacityButton = document.getElementById('opacityButton');
+    getOpacityButton.addEventListener('click', function(){
+        activeButton = 'opacity'
+    })
+};
+
+function randomColor() {
+    const getRandomButton = document.getElementById('randomButton');
+        getRandomButton.addEventListener('click', function(){
+        activeButton = 'random'
+    });
+};
+
+function blackColor() {
+    const getBlackButton = document.getElementById('blackButton');
+        getBlackButton.addEventListener('click', function(){
+        activeButton = 'black'
+    });
+};
+
 baseGridQuantity();
 resetGrid();
 changeGridSize();
+opacityChange();
+randomColor();
+blackColor();
